781628bdb989eea6a03b23ffa02ef35b7f5b1d39
Milan <milanprsingh@gmail.com>
2020-08-16 22:04:37 -0700

    Some jaaanky level generation

---
+++ 1-Hour-Game-2/1 Hour Game Jam 2/Assets/Scripts/Floor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public static float maxHoleWidth;
+    public static float minHoleWidth;
+    public static float floorThickness;
+    public static float scale;
+
+    LevelGenerator levelGenerator;
+
+    // x in [0,1];  y in world space 
+    float holeWidth;
+    float holeX;
+    float height;
+
+    GameObject leftSide;
+    GameObject rightSide;
+    BoxCollider trigger;
+
+    void Start()
+    {
+        Vector3 position = transform.position;
+        holeWidth = Random.Range(minHoleWidth, maxHoleWidth);
+        holeX = Random.Range(holeWidth / 2.0f, 1 - holeWidth / 2.0f);
+        
+        // left side
+        GameObject left = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        left.transform.localScale = new Vector3(scale, floorThickness, 1.0f);
+        float leftX = scale*(-1.0f/2 - holeWidth/2 - (0.5f-holeX));
+        left.transform.position = new Vector3(position.x + leftX, position.y, position.z);
+
+        // right side
+        GameObject right = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        right.transform.localScale = new Vector3(scale, floorThickness, 1.0f);
+        float rightX = scale + leftX + scale*holeWidth;
+        right.transform.position = new Vector3(position.x + rightX, position.y, position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ 1-Hour-Game-2/1 Hour Game Jam 2/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cam;
+
+    public GameObject floorPrefab;
+
+    public static LevelGenerator levelGenerator = null;
+
+    LinkedList<GameObject> floorList;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.IsTrue(levelGenerator == null, "levelGenerator is a smelly ugly singleton made with sticks");
+        levelGenerator = this;
+
+        floorList = new LinkedList<GameObject>();
+
+        Floor.maxHoleWidth = 0.25f;
+        Floor.minHoleWidth = 0.2f;
+        Floor.floorThickness = 0.25f;
+        Floor.scale = 2 * cam.orthographicSize * cam.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}

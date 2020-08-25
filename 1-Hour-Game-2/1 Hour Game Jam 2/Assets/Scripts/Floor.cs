6c29a5dce422317d90f2c50aa10eee1d5598fb4b
Milan <milanprsingh@gmail.com>
2020-08-24 19:06:37 -0700

    Floor generation first pass

--- 1-Hour-Game-2/1 Hour Game Jam 2/Assets/Scripts/Floor.cs
+++ 1-Hour-Game-2/1 Hour Game Jam 2/Assets/Scripts/Floor.cs
@@ -8,8 +8,7 @@ public class Floor : MonoBehaviour
     public static float minHoleWidth;
     public static float floorThickness;
     public static float scale;
-
-    LevelGenerator levelGenerator;
+    public static LevelGenerator levelGenerator;
 
     // x in [0,1];  y in world space 
     float holeWidth;
@@ -20,16 +19,21 @@ public class Floor : MonoBehaviour
     GameObject rightSide;
     BoxCollider trigger;
     float triggerScale;
+    bool hasGeneratedNext = false;
 
     void Start()
     {
         leftSide = GameObject.CreatePrimitive(PrimitiveType.Cube);
         rightSide = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        triggerScale = -1.0f;
+        trigger = gameObject.AddComponent<BoxCollider>();
+        trigger.isTrigger = true;
+        trigger.transform.localScale = new Vector3(scale, floorThickness, 1.0f);
+        Randomize();
     }
 
     public void Randomize()
     {
+        hasGeneratedNext = false;
         Vector3 position = transform.position;
         holeWidth = Random.Range(minHoleWidth, maxHoleWidth);
         holeX = Random.Range(holeWidth / 2.0f, 1 - holeWidth / 2.0f);
@@ -43,14 +47,12 @@ public class Floor : MonoBehaviour
         rightSide.transform.position = new Vector3(position.x + rightX, position.y, position.z);
     }
 
-    private void Update()
-    {
-        
-    }
-
-
     private void OnTriggerEnter(Collider other)
     {
-        levelGenerator.GenerateFloor();
+        if (!hasGeneratedNext)
+        {
+            levelGenerator.GenerateFloor();
+            hasGeneratedNext = true;
+        }
     }
 }
--- 1-Hour-Game-2/1 Hour Game Jam 2/Assets/Scripts/LevelGenerator.cs
+++ 1-Hour-Game-2/1 Hour Game Jam 2/Assets/Scripts/LevelGenerator.cs
@@ -9,9 +9,7 @@ public class LevelGenerator : MonoBehaviour
     public Camera cam;
 
     public GameObject floorPrefab;
-
-    public static LevelGenerator levelGenerator = null;
-
+    
     private float floorY = 0.0f;
 
     LinkedList<GameObject> floorList;
@@ -22,16 +20,15 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Assert.IsTrue(levelGenerator == null, "levelGenerator is a smelly ugly singleton made with sticks");
-        levelGenerator = this;
-
         floorList = new LinkedList<GameObject>();
 
         Floor.maxHoleWidth = 0.25f;
         Floor.minHoleWidth = 0.2f;
         Floor.floorThickness = 0.25f;
         Floor.scale = 2 * cam.orthographicSize * cam.aspect;
+        Floor.levelGenerator = this;
 
+        GenerateFloor();
     }
 
     // Update is called once per frame
@@ -44,16 +41,21 @@ public class LevelGenerator : MonoBehaviour
     {
         if (floorList.Count > maxFloors)
         {
-            GameObject firstFloor = floorList.First.Value;
+            GameObject floor = floorList.First.Value;
             floorList.RemoveFirst();
-            firstFloor.transform.position = new Vector3(firstFloor.transform.position.x, floorY, firstFloor.transform.position.z);
-            firstFloor.GetComponent<Floor>().Randomize();
-            floorList.AddLast(firstFloor);
+            floor.transform.position = new Vector3(transform.position.x, floorY, transform.position.z);
+            floor.GetComponent<Floor>().Randomize();
+            floorList.AddLast(floor);
         }
+        else
+        {
+            GameObject floor = new GameObject(string.Format("Floor {0}", floorList.Count + 1));
+            floor.transform.position = new Vector3(transform.position.x, floorY, transform.position.z);
+            floor.AddComponent<Floor>();
+            floorList.AddLast(floor);
+
+        }
+        floorY -= Random.Range(minFloorDistance, maxFloorDistance);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        
-    }
 }
